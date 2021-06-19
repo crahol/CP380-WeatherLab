ea414c2011c7d1695ead11ce673fdc423219f602
crahol <raholchaitali20@gmail.com>
2021-06-18 20:59:49 -0400

    Completed the activities

--- WeatherLab/Program.cs
+++ WeatherLab/Program.cs
@@ -12,8 +12,10 @@ namespace WeatherLab
         {
             var measurements = new WeatherSqliteContext(dbfile).Weather;
 
-            var total_2020_precipitation = ?? TODO ??
-            Console.WriteLine($"Total precipitation in 2020: {total_2020_precipitation} mm\n");
+            var total_2020_precipitation = measurements
+                .Where(data => data.year == 2020)
+                .Select(r => r.precipitation);
+            Console.WriteLine($"Total precipitation in 2020: {total_2020_precipitation.Sum()} mm\n");
 
             //
             // Heating Degree days have a mean temp of < 18C
@@ -41,13 +43,45 @@ namespace WeatherLab
             //      $"{x:d4}"
             //
             Console.WriteLine("Year\tHDD\tCDD");
-
             // ?? TODO ??
+            var group = measurements
+                .GroupBy(r => r.year)
+                .Select(grp => new
+                {
+                    year = grp.Key,
+                    cdd = grp.Where(g => g.meantemp >= 18).Count(),
+                    hdd = grp.Where(g => g.meantemp < 18).Count()
+                });
+
+            foreach (var g in group)
+            {
+                Console.WriteLine($"{g.year}\t{g.hdd}\t{g.cdd}");
+            }
 
             Console.WriteLine("\nTop 5 Most Variable Days");
             Console.WriteLine("YYYY-MM-DD\tDelta");
 
             // ?? TODO ??
+            var mostVariableDays = measurements
+                .Select(row => new
+                    {
+                        date = $"{row.year}-{row.month:d2}-{row.day:d2}",
+                        diff = (row.maxtemp - row.mintemp)
+                    })
+                .OrderByDescending(r => r.diff);
+
+            int count = 0;
+            foreach (var m in mostVariableDays)
+            {
+                if (count < 5)
+                {
+                    Console.WriteLine($"{m.date}\t{m.diff}");
+                    count++;
+                } else
+                {
+                    break;
+                }
+            }
         }
     }
 }
